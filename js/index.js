0ce5825f34b4fdf31c44fb79343c8c119da91e4a
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-09-03 14:31:41 +0100

    drag, drop, focus

--- js/index.js
+++ js/index.js
@@ -3,26 +3,53 @@ const links = document.querySelectorAll('a');
 const introH = document.querySelector('.intro h2')
 const body = document.body;
 const button = document.querySelectorAll('.btn');
+const letsGoImg = document.querySelector('.content-section img');
+const advGoImg = document.querySelector('.inverse-content img');
+const foot = document.querySelector('.footer');
+const destH = document.querySelectorAll('.destination h4');
 
-links.forEach(link => {
-    link.addEventListener('mouseover', () => {
+foot.addEventListener('dragover', e =>{
+    e.preventDefault();
+})
+
+destH.forEach(dH => {
+    dH.addEventListener('mouseover', () => {
         console.log('I am hovering');
-        link.setAttribute("style", "color: gold; background-color: purple; padding: 2px; margin: 3px;");
+        dH.setAttribute("style", "color: gold; background-color: purple; padding: 2px; margin: 3px;");
     });
-    link.addEventListener('mouseleave', () => {
+    dH.addEventListener('mouseleave', () => {
         console.log('I have left');
-        link.setAttribute("style", "color: ''; background-color: ''; padding-bottom: 2px; margin: 3px;");
+        dH.setAttribute("style", "color: ''; background-color: ''; padding-bottom: 2px; margin: 3px;");
     });
 });
 
 body.addEventListener('keydown', () =>{
     console.log('KEY DOWN!!!');
     introH.textContent = "STOP PUSHING KEYS!"
+    introH.style.color = 'red';
 });
 
-body.addEventListener('wheel', (event) => {
+body.addEventListener('wheel', () => {
     console.log('On and on, the wheel turns...');
     button.forEach(buttn =>{
         buttn.style.backgroundColor = 'purple';
+    })
+})
+
+body.addEventListener('drag', () =>{
+    console.log('Alert! SOmething is being DRagged!')
+    letsGoImg.setAttribute('src', 'img/fun.jpg')
+    advGoImg.setAttribute('src', 'img/adventure.jpg');
+})
+
+body.addEventListener('drop', () =>{
+    console.log('It has been dropped. Gratitude!!!')
+    letsGoImg.setAttribute('src', 'img/adventure.jpg')
+    advGoImg.setAttribute('src', 'img/fun.jpg');
+})
+
+links.forEach(link => {
+    link.addEventListener('focus', (event) => {
+        event.target.style.color = "purple";
     })
 })
