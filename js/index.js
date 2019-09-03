f15532274f3a19d1898006491c266090bf01a017
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-09-03 13:05:27 +0100

    mouseover, keydown, wheel

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+const links = document.querySelectorAll('a');
+const introH = document.querySelector('.intro h2')
+const body = document.body;
+const button = document.querySelectorAll('.btn');
+
+links.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        console.log('I am hovering');
+        link.setAttribute("style", "color: gold; background-color: whitesmoke; padding: 2px; margin: 3px;");
+    });
+    link.addEventListener('mouseleave', () => {
+        console.log('I have left');
+        link.setAttribute("style", "color: ''; background-color: ''; padding-bottom: 2px; margin: 3px;");
+    });
+});
+
+body.addEventListener('keydown', () =>{
+    console.log('KEY DOWN!!!');
+    introH.textContent = "STOP PUSHING KEYS!"
+});
+
